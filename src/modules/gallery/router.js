23545a8881eda0b4c4eb864e9e5d519c2d46c5dd
harendrsainh <haren111990@gmail.com>
2026-08-01 21:54:23 +0530

    router update for get image

--- src/modules/gallery/router.js
+++ src/modules/gallery/router.js
@@ -8,12 +8,12 @@ const GalleryRouter = express.Router()
 GalleryRouter.get("/getImageData",galleryController.getImageData)
 GalleryRouter.get("/getDropDownList",galleryController.getDropDownList)
 GalleryRouter.get("/getYear/:EventName",galleryController.getFunctionYear)
+GalleryRouter.post("/getYearImage",galleryController.getYearImage)
 
 GalleryRouter.post("/uploadImage",upload.single("image"),authMiddleware,galleryController.uploadImage)
 GalleryRouter.post("/dropdowItem",authMiddleware,galleryController.dropDownItem)
 GalleryRouter.post("/DeleteEvent",authMiddleware,galleryController.DeleteEvent)
 GalleryRouter.post("/editdropdowItem",authMiddleware,galleryController.EditdropdowItem)
-GalleryRouter.post("/getYearImage",authMiddleware,galleryController.getYearImage)
 GalleryRouter.post("/getEditImages",authMiddleware,galleryController.getEditImages)
 GalleryRouter.delete("/deleteImage/:imageId",authMiddleware,galleryController.deleteImage)
 export default GalleryRouter
